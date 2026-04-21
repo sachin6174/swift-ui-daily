f9c6e7e6cd00a241d984d97765c2ea076a5e99c8
sachinkumar <sachinmehtab@gmail.com>
2026-04-22 01:38:38 +0530

    first commit

---
+++ AddingText/AddingText/AddingTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddingTextApp.swift
+//  AddingText
+//
+//  Created by sachin kumar on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AddingTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AddingText/AddingText/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  AddingText
+//
+//  Created by sachin kumar on 22/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("shree ganesh")
+    }
+}
+
+#Preview {
+    ContentView()
+}
